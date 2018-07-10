67c09d7237458d0bca19f10cbb3b2adbc5b55bc1
Michal Kuffa <michal@bynder.com>
2018-07-10 22:30:30 +0200

    Add handler code

---
+++ elastalert_lambda.py
@@ -0,0 +1,35 @@
+import os
+import datetime
+import shlex
+
+from elastalert import elastalert
+
+
+def handler(event, context):
+    print("Starting up ElastAlert")
+    args = shlex.split(event.get('ARGS', os.getenv('ARGS', '')))
+
+    if '--end' not in args:
+        args.extend([
+            '--end',
+            datetime.datetime.utcnow().isoformat(),
+        ])
+
+    if '--config' not in args:
+        args.extend(['--config', 'config.yaml'])
+
+    print("Using arguments: `%s`" % args)
+    elastalert.main(args)
+
+
+if __name__ == "__main__":
+    """
+    Purely for local testing
+    """
+    import json
+    import sys
+    import select
+    event = {}
+    if select.select([sys.stdin,],[],[],0.0)[0]:
+        event = json.loads(sys.stdin.read())
+    handler(event, None)
