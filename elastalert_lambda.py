a22371000323edd71bd0beee18a3d1d33eaf32e3
Krzysztof Szyper <45788587+Krzysztof-Szyper-Epam@users.noreply.github.com>
2019-01-25 13:13:20 +0100

    Create indices automatically - essential for IaaC approach

--- elastalert_lambda.py
+++ elastalert_lambda.py
@@ -1,9 +1,18 @@
 import os
+import sys
 import datetime
 import shlex
 
 from elastalert import elastalert
+from elastalert import create_index
 
+class TmpSysArgv(object):
+
+    def __enter__(self):
+        self.orig_argv = sys.argv
+
+    def __exit__(self, *args, **kwargs):
+        sys.argv = self.orig_argv
 
 def handler(event, context):
     print("Starting up ElastAlert")
@@ -19,10 +28,30 @@ def handler(event, context):
         args.extend(['--config', 'config.yaml'])
 
     print("Using arguments: `%s`" % args)
+
+    if "EA_CREATE_INDEX" in os.environ:
+
+        if "EA_CREATE_INDEX_ARGS" in os.environ:
+            ci_args = ["elastalert-create-index"] + shlex.split(
+                os.getenv("EA_CREATE_INDEX_ARGS"))
+        else:
+            config_index = args.index("--config")
+            ci_args = [
+                "elastalert-create-index",
+                args[config_index],
+                args[config_index + 1]
+            ]
+
+        print("Creating index for elastalert with args: %s" % (ci_args, ))
+        with TmpSysArgv():
+            sys.argv = ci_args
+            create_index.main()
+
     try:
         elastalert.main(args)
     except SystemExit as exc:
         if exc.args[0] == 0:
+            print("ElastAlert run successfully!")
             return
         raise
 
